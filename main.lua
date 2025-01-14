fb5c8c922ab85bcc4e5af4bdbac7f869c92ad7d4
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-01-14 14:08:02 -0500

    prototyped the bones

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
+-- Atlasses
 SMODS.Atlas {key = "Placeholder", px = 71, py = 95, path = "Placeholder.png"}
-SMODS.Atlas {key = "TheClaw", px = 71, py = 95, path = "TheClaw.png"}
-SMODS.Atlas {key = "TheFeather", px = 71, py = 95, path = "TheFeather.png"}
+SMODS.Atlas {key = "TheClaw", px = 71, py = 95, path = "Claw2.png"}
+SMODS.Atlas {key = "TheFeather", px = 71, py = 95, path = "TheFeather2.png"}
+SMODS.Atlas {key = "TheEye", px = 71, py = 95, path = "TheEye.png"}
+SMODS.Atlas {key = "TheBones", px = 71, py = 95, path = "TheBones.png"}
+
+
 -- The Claw
 SMODS.Joker {
     key = 'theclaw',
@@ -47,7 +52,7 @@ SMODS.Joker {
     },
     config = {},
     rarity = 3,
-    atlas = "TheFeather", -- TODO: Make art
+    atlas = "TheFeather",
     pos = {x = 0, y = 0},
     cost = 4,
     blueprint_compat = true,
@@ -84,18 +89,57 @@ SMODS.Joker {
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.money } }
     end,
-    config = { extra = { money = -9}},
+    config = { extra = { money = -9} },
     rarity = 3,
-    altas = "Placeholder", -- TODO: Make art
+    atlas = "TheEye",
     pos = {x = 0, y = 0},
     cost = 4,
     blueprint_compat = false,
     calculate = function(self, card, context)
         if context.buying_card and not context.blueprint then
             context.card:set_edition("e_negative")
-            ease_dollars(card.ability.extra.money, true)
+            ease_dollars(card.ability.extra.money)
             card_eval_status_text(card, 'extra', nil, nil, nil, {message = '$'..card.ability.extra.money, colour = G.C.GOLD})
             return
+        end
+    end
+}
+
+-- The Bones
+SMODS.Joker {
+    key = "thebones",
+    loc_txt = {
+        name = "The Bones",
+        text = {
+            "Every glass card", 
+            "in played hand {C:red}breaks{}," ,
+            "adds {C:attention}double rank{}", 
+            "to mult"
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return { vars = {} }
+    end,
+    config = { },
+    rarity = 3,
+    atlas = "TheBones", 
+    pos = {x = 0, y = 0},
+    cost = 4,
+    blueprint_compat = false,
+    calculate = function(self, card, context)
+        if context.joker_main and not context.blueprint then
+            local total_mult = 0
+            for _, _card in ipairs(context.full_hand) do
+                local card_rank = _card:get_id()
+                if _card.ability.effect == 'glass' then
+                    print(card_rank)
+                    _card:shatter()
+                    total_mult = total_mult + 2 * card_rank
+                end
+            end
+            return {
+                mult = total_mult
+            }
         end
     end
 }
