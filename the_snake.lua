367a51e70e21137e90c296aa254b91fc6031dffd
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-01-17 12:41:45 -0500

    set up the snake

---
+++ the_snake.lua
@@ -0,0 +1,11 @@
+--[[
+The Snake
+
+This set's primary focus will be cards that accrue value over time.
+
+The Fang: X4 mult per joker (not including self), 1 in 4 chance to destroy a random joker at the end of round
+]]
+
+SMODS.Atlas {key = "placeholder", px = 71, py = 95, path = "Placeholder.png"}
+
+
